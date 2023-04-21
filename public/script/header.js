f653b2809183dc5e0e4472fb4093540d863b0e27
Reniel Galerio <rcgalerio@gmail.com>
2023-04-21 20:22:25 +0800

    update: added animations and change entourage details

--- public/script/header.js
+++ public/script/header.js
@@ -59,4 +59,21 @@ function onHeaderScroll() {
             }
         }
     }
+
+    reveal()
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal")
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var elementTop = reveals[i].getBoundingClientRect().top
+      var elementVisible = 0
+  
+      if (elementTop < vh - elementVisible) {
+        reveals[i].classList.add("active")
+      } else {
+        reveals[i].classList.remove("active")
+      }
+    }
 }
